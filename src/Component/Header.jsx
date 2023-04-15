25b51957ad83f237a9fcf3cf5efc6b037414eb0a
miyad <mhmiyad6565@gmail.com>
2023-04-15 21:35:20 +0600

    add daisy ui and firebase

---
+++ src/Component/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div className="navbar bg-primary text-primary-content">
+  <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+</div>
+
+
+
+    </div>
+  )
+}
+
+export default Header
